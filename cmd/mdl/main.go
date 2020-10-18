921532ff27c14abaa5b3a2fa8625970835025add
Raphael Simon <simon.raphael@gmail.com>
2020-10-17 20:27:20 -0700

    Fix command line parsing.
    Add "serve" and "gen" commands to mdl.

--- cmd/mdl/main.go
+++ cmd/mdl/main.go
@@ -6,7 +6,9 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path"
 	"path/filepath"
 	"strings"
 
@@ -24,54 +26,87 @@ func fileExists(filename string) bool {
 }
 
 func main() {
-	out := flag.String("out", codegen.Gendir, "set diagram persistence directory")
-	port := flag.Int("port", 8080, "set local HTTP port")
-	debug := flag.Bool("debug", false, "pring debug output")
-	help := flag.Bool("help", false, "print this help and exit")
-	version := flag.Bool("version", false, "print version information and exit")
-	devmode := os.Getenv("DEVMODE") == "1"
-	flag.Parse()
+	var (
+		fs      = flag.NewFlagSet("flags", flag.ContinueOnError)
+		out     = fs.String("out", codegen.Gendir, "set output directory used to save model JSON and SVGs")
+		port    = fs.Int("port", 8080, "set local HTTP port used to serve diagram editor")
+		debug   = fs.Bool("debug", false, "pring debug output")
+		devmode = os.Getenv("DEVMODE") == "1"
+	)
 
-	if *help {
-		showUsage()
-		os.Exit(0)
+	var (
+		cmd   string
+		fpath string
+		idx   int
+	)
+	for _, arg := range os.Args[1:] {
+		idx++
+		switch cmd {
+		case "":
+			cmd = arg
+		case "serve", "gen":
+			if !strings.HasPrefix(arg, "-") {
+				fpath = arg
+				idx++
+			}
+			goto done
+		default:
+			goto done
+		}
+	}
+done:
+	fs.Parse(os.Args[idx:])
+
+	if cmd == "gen" || cmd == "serve" {
+		// Initialize saved views directory if needed.
+		*out, _ = filepath.Abs(*out)
+		err := os.MkdirAll(*out, 0777)
+		if err != nil {
+			fail(err.Error())
+		}
 	}
 
-	if *version {
+	var err error
+	switch cmd {
+	case "gen":
+		if fpath == "" {
+			fail(`missing PACKAGE argument, use "--help" for usage.`)
+		}
+		var b []byte
+		b, err = gen(fpath, *debug)
+		if err == nil {
+			err = ioutil.WriteFile(path.Join(*out, "model.json"), b, 0644)
+		}
+	case "serve":
+		err = serve(*out, fpath, *port, devmode, *debug)
+	case "version":
 		fmt.Printf("%s %s\n", os.Args[0], model.Version())
-		os.Exit(0)
+	case "help":
+		showUsage(fs)
+	default:
+		showUsage(fs)
+		os.Exit(1)
 	}
-
-	if len(os.Args) < 2 {
-		fail(`missing PACKAGE argument, use "--help" for usage.`)
-	}
-
-	pkg := os.Args[1]
-	if strings.HasPrefix(pkg, "-") {
-		fail(`missing PACKAGE argument, use "--help" for usage.`)
-	}
-
-	// Initialize saved views directory if needed.
-	outDir, _ := filepath.Abs(*out)
-	err := os.MkdirAll(outDir, 0777)
 	if err != nil {
 		fail(err.Error())
 	}
+}
 
+func serve(out, pkg string, port int, devmode, debug bool) error {
 	// Retrieve initial design and create server.
-	b, err := gen(pkg, *debug)
+	b, err := gen(pkg, debug)
 	if err != nil {
-		fail(err.Error())
+		return err
 	}
 	var design mdl.Design
 	if err := json.Unmarshal(b, &design); err != nil {
-		fail("failed to load design: " + err.Error())
+		return fmt.Errorf("failed to load design: %s", err.Error())
 	}
 	s := NewServer(&design)
 
 	// Update server whenever design changes on disk.
 	err = watch(pkg, func() {
-		b, err := gen(pkg, *debug)
+		b, err := gen(pkg, debug)
 		if err != nil {
 			fmt.Println("error parsing DSL:\n" + err.Error())
 			return
@@ -83,18 +118,20 @@ func main() {
 		s.SetDesign(&design)
 	})
 	if err != nil {
-		fail(err.Error())
+		return err
 	}
 
-	err = s.Serve(outDir, devmode, *port)
-	fail(err.Error())
+	return s.Serve(out, devmode, port)
 }
 
-func showUsage() {
-	fmt.Fprintf(os.Stderr, "Usage: %s PACKAGE [FLAGS].\n", os.Args[0])
-	fmt.Fprintln(os.Stderr, "Start a HTTP server that serves a graphical editor for the design described in PACKAGE.")
-	fmt.Fprintf(os.Stderr, "PACKAGE must be the import path to a Go package containing Model DSL.\n\n")
-	flag.PrintDefaults()
+func showUsage(fs *flag.FlagSet) {
+	fmt.Fprintln(os.Stderr, "Usage:")
+	fmt.Fprintf(os.Stderr, " - %s serve PACKAGE [FLAGS].\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "   Start a HTTP server that serves a graphical editor for the design described in PACKAGE.\n")
+	fmt.Fprintf(os.Stderr, " - %s gen PACKAGE [FLAGS].\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "   Generate a JSON representation of the design described in PACKAGE.\n\n")
+	fmt.Fprintf(os.Stderr, "   PACKAGE must be the import path to a Go package containing Model DSL.\n\n")
+	fs.PrintDefaults()
 }
 
 func fail(format string, args ...interface{}) {
